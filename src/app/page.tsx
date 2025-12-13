6114bd2e12c7483a429d3b070fa85bfdc1c25c40
Luca Piccone <lpiccone19@gmail.com>
2025-12-13 18:37:51 -0500

    remove sign in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,6 @@ import Image from 'next/image'
 export default function Home() 
 {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
-  const [data, setData] = useState(null);
-
   
 
   return (
